f3d4d064e04c1c32e8098bce4e8cb33d67eca620
muthukumardharunya-collab <muthukumardharunya@gmail.com>
2026-02-16 12:08:11 +0530

    Modified Script

--- PythonProject1/Assignment1_Task2.py
+++ PythonProject1/Assignment1_Task2.py
@@ -1,19 +1,5 @@
-"""
-Task 2: Create a Personalized Greeting
-Problem Statement: Write a Python program that:
-1.  Takes a user's first name and last name as input.
-2.  Concatenates the first name and last name into a full name.
-3.  Prints a personalized greeting message using the full name.
-Expected Output:
-The program should output a greeting like:
-Hello, my name is
-"""
-
-"""Task2"""
-
+#Task2-Create the Personalized greeting
 a1 = input("what is your first name: ")
-b1 = input("What is your second name! ")
-
-print("Hello"),input(a1+b1)
-
-print("Welcome to the Python language")
+b1 = input("What is your second name: ")
+name = a1 + " " + b1 + "!"
+print("Hello,",a1,b1,"! Welcome to the Python Program.")
